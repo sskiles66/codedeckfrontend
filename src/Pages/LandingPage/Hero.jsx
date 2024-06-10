c3d76b2d37b14173ee5b398d9e2a6dbd4b1c13d1
Stephen Skiles <stephentskiles@hotmail.com>
2024-06-09 18:53:38 -0600

    Added gradient hero

--- src/Pages/LandingPage/Hero.jsx
+++ src/Pages/LandingPage/Hero.jsx
@@ -5,30 +5,22 @@ import React from "react";
 
 export default function Hero() {
 
-  
 
-  return (
-    <div className="hero-container">
-        <div className="hero-img">
+
+    return (
+        <div className="hero-container">
+            {/* <div className="hero-img">
             <img src="hero2.jpg"/>
 
             <div className="heroTitle">
                 <h1>CodeDeck</h1>
             </div>
 
-            {/* <div className="heroSearchContainer">
-                <input
-                    type="text"
-                    className="heroSearch"
-                    placeholder="Search data..."
-                    // value={value}
-                    // onChange={(e) => {
-                    //   setValue(e.target.value); // Update the search input value
-                    // }}
-                />
-            </div> */}
+        </div> */}
 
+            <div className="heroTitle">
+                <h1>CodeDeck</h1>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
