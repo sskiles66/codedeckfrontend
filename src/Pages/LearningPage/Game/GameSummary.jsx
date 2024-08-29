d925a0adde7a27b6ec58a16f0781d20137457ac5
Stephen Skiles <stephentskiles@hotmail.com>
2024-08-29 12:16:40 -0600

    Games played is functional

--- src/Pages/LearningPage/Game/GameSummary.jsx
+++ src/Pages/LearningPage/Game/GameSummary.jsx
@@ -6,75 +6,88 @@ import { GameContext } from "./GameContext";
 
 export default function GameSummary() {
 
-    const { score, timer, isGameOver, setIsGameOver, setQuestionNum, setScore, setTimer  } = useContext(GameContext);
+  const { score, timer, isGameOver, setIsGameOver, setQuestionNum, setScore, setTimer } = useContext(GameContext);
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
-    const [name, setName] = useState();
+  const [name, setName] = useState();
 
-    const [userId, setUserId] = useState();
+  const [userId, setUserId] = useState();
 
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-    useEffect(() => {
-        function setGameValues(){
-            if (isAuthenticated){
-                setName(user.given_name);
-                setUserId(user.sub);
-            }else{
-                setName("Guest");
-                setUserId("");
-            }
-        }
-        setGameValues();
-    }, [isAuthenticated]);
-
-    function restartGame(e){
-        e.preventDefault();
-        setIsGameOver(false);
-        setQuestionNum(1);
-        setScore(0);
-        setTimer(0);
-        setIsButtonDisabled(false);
-    }
-
-    async function handleGameCreation(e) {
-        e.preventDefault();
-        setIsButtonDisabled(true);
-        try {
-          const createGameResponse = await axios.post(
-            'http://localhost:4000/api/learningPage/create-game',
-            {
-              learning_page_id: id,
-              user_id: userId,
-              user_first_name: name,
-              score: score,
-              time: timer,
-            },
-          );
-    
-          // console.log('User data saved:', createUserResponse.data);
-        //   navigate(`/learningPage/${createUserResponse.data._id}`);
-        } catch (error) {
-          console.error('Error saving user data:', error);
-        }
+  useEffect(() => {
+    function setGameValues() {
+      if (isAuthenticated) {
+        setName(user.given_name);
+        setUserId(user.sub);
+      } else {
+        setName("Guest");
+        setUserId("");
       }
+    }
+    setGameValues();
+  }, [isAuthenticated]);
 
+  function restartGame(e) {
+    e.preventDefault();
+    setIsGameOver(false);
+    setQuestionNum(1);
+    setScore(0);
+    setTimer(0);
+    setIsButtonDisabled(false);
+  }
 
-
-    return (
-          isGameOver ? (
-            <div className="gameSummary">
-              <h2>Game Results</h2>
-              <p>Final Score: {score}</p>
-              <p>Final Time: {timer}</p>
-              <button disabled={isButtonDisabled} onClick={handleGameCreation}>Save Score</button>
-              <button onClick={restartGame}>Click To Play Again</button>
-            </div>
-          ) : (
-              ""
-          )
+  async function handleGameCreation(e) {
+    e.preventDefault();
+    setIsButtonDisabled(true);
+    try {
+      const createGameResponse = await axios.post(
+        'http://localhost:4000/api/learningPage/create-game',
+        {
+          learning_page_id: id,
+          user_id: userId,
+          user_first_name: name,
+          score: score,
+          time: timer,
+        },
       );
+
+      // console.log('User data saved:', createUserResponse.data);
+      //   navigate(`/learningPage/${createUserResponse.data._id}`);
+    } catch (error) {
+      console.error('Error saving user data:', error);
+    }
+    try {
+      const accessToken = await getAccessTokenSilently();
+      const response = await axios.patch(`http://localhost:4000/api/learningPage/increment-user-games-played/${userId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            // Add any other custom headers here if needed
+          },
+        });
+    } catch (error) {
+      console.error('Error making PATCH request:', error);
+    }
+  }
+
+
+
+  return (
+    isGameOver ? (
+      <div className="gameSummary">
+        <h2>Game Results</h2>
+        <p>Final Score: {score}</p>
+        <p>Final Time: {timer}</p>
+        <button disabled={isButtonDisabled} onClick={handleGameCreation}>Save Score</button>
+        <button onClick={restartGame}>Click To Play Again</button>
+      </div>
+    ) : (
+      ""
+    )
+  );
 }
