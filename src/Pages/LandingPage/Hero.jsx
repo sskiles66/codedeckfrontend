f19b55e1d3b0fc75aeb36ee607498cb461202f61
Stephen Skiles <stephentskiles@hotmail.com>
2024-08-31 18:46:03 -0600

    Made hero a little better

--- src/Pages/LandingPage/Hero.jsx
+++ src/Pages/LandingPage/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
         <div className="hero-container">
             <div className="heroContentContainer">
                 <div className="heroText">
-                    <h1>CodeDeck</h1>
-                    <p>Students reinforce learning.</p>
-                    <p>Teachers reinforce teaching.</p>
-                    <p>Promotes friendly competition in classes.</p>
+                    <h1>Make Learning Efficient</h1>
+                    <p>Reinforce concepts with analogies, games, and interactive tools.</p>
+                    <p>Teachers and/or professors can make their own learning pages for their classes and curriculumns.</p>
+                    <p>Students can participate and see how they rank among their peers.</p>
                 </div>
             </div>
         </div>
